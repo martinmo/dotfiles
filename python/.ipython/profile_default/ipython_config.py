2ea6fefeb600dc45b761695cd789799f5a3646c5
Martin Morgenstern <mail@mmorgenstern.net>
2016-08-01 10:37:30 +0200

    Load favorite imports at IPython startup :snake:

--- python/.ipython/profile_default/ipython_config.py
+++ python/.ipython/profile_default/ipython_config.py
@@ -1,3 +1,10 @@
 c = get_config()
 
 c.InteractiveShell.confirm_exit = False
+c.InteractiveShellApp.exec_lines = [
+    'import itertools',
+    'import math',
+    'import re',
+    'import sys',
+    'from pathlib import Path',
+]
