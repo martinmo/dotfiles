db834decc59e41a57b62bd28915ed2f6ec289243
Martin Morgenstern <martin.morgenstern1@tu-dresden.de>
2022-04-22 20:47:30 +0200

    Make vim keybindings usable in Firefox

--- set_macos_defaults.sh
+++ set_macos_defaults.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
+defaults write org.mozilla.firefox ApplePressAndHoldEnabled -bool false
 defaults write org.gpgtools.common UseKeychain -bool false
