f2baa2d86a9b0137cd9f93c2b09c09f9c2909ccf
Martin Morgenstern <mail@mmorgenstern.net>
2024-01-17 18:46:39 +0100

    Load provided fzf configs from OS dependent locations

--- zsh/.zsh/fuzzyfinder.zsh
+++ zsh/.zsh/fuzzyfinder.zsh
@@ -1,4 +1,13 @@
-if [[ -d "$HOMEBREW_PREFIX/opt/fzf" ]]; then
-    source "$HOMEBREW_PREFIX/opt/fzf/shell/completion.zsh"
-    source "$HOMEBREW_PREFIX/opt/fzf/shell/key-bindings.zsh"
+_load_fzf_config() {
+    local basedir=$1
+    if [[ -d "$basedir" ]]; then
+        source "$basedir/completion.zsh"
+        source "$basedir/key-bindings.zsh"
+    fi
+}
+
+if [[ -n "$HOMEBREW_PREFIX" ]]; then
+    _load_fzf_config "$HOMEBREW_PREFIX/opt/fzf/shell"
+else
+    _load_fzf_config /usr/share/doc/fzf/examples
 fi
